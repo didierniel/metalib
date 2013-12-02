68452888817fc51fe577f2e7862d68f0fa111f40
Robin Good Fellow <didier.niel@info-sesame.com>
2013-12-02 15:56:18 +0100

    desc

---
+++ metalib-web/src/main/webapp/personslist-web/navigation-panel.view.js
@@ -0,0 +1,19 @@
+sap.ui.jsview("personlist-web.navigation-panel", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf personlist-web.navigation-panel
+	*/ 
+	getControllerName : function() {
+		return "personlist-web.navigation-panel";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf personlist-web.navigation-panel
+	*/ 
+	createContent : function(oController) {
+
+	}
+
+});
